6d8acb640cdb4dcf813b65346ed3ca81e0fc1a6c
Clayton Williams <worldofclayton@gmail.com>
2017-10-05 15:00:20 -0400

    Setup

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    define("_VALID_PHP", true);
+    require_once($_SERVER['DOCUMENT_ROOT']."/assets/lib/init.php");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<head>
+    <title>PHP - MVC</title>
+    <meta name="description" content="Stuff">
+    <meta name="keywords" content="Stuff">
+    <!-- Core Styling -->
+    <link href="/assets/style/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=9" /><!-- Dotted Outline fix -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/assets/style/theme.css?rand=<?php echo rand(0, 100000); ?>">
+</head>
+
+<div id="wrapper">
+
+</div>
+
+
+</html>
+
+<script src="/assets/scripts/jquery/jquery-3.2.1.min.js"></script>
+<script src="/assets/scripts/bootstrap/bootstrap.min.js"></script><!-- Bootstrap after jQuery -->
+<script>
+
+
+</script>
