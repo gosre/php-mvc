42e7d093f264eb83e02934983053bfb645d46695
Clayton Williams <worldofclayton@gmail.com>
2017-10-05 16:22:34 -0400

    Reorganization

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     define("_VALID_PHP", true);
-    require_once($_SERVER['DOCUMENT_ROOT']."/assets/lib/init.php");
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/public/lib/init.php");
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <head>
@@ -8,12 +8,12 @@
     <meta name="description" content="Stuff">
     <meta name="keywords" content="Stuff">
     <!-- Core Styling -->
-    <link href="/assets/style/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="/public/style/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="X-UA-Compatible" content="IE=9" /><!-- Dotted Outline fix -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/assets/style/theme.css?rand=<?php echo rand(0, 100000); ?>">
+    <link rel="stylesheet" href="/public/style/theme.css?rand=<?php echo rand(0, 100000); ?>">
 </head>
 
 <div id="wrapper">
@@ -23,8 +23,8 @@
 
 </html>
 
-<script src="/assets/scripts/jquery/jquery-3.2.1.min.js"></script>
-<script src="/assets/scripts/bootstrap/bootstrap.min.js"></script><!-- Bootstrap after jQuery -->
+<script src="/public/scripts/jquery/jquery-3.2.1.min.js"></script>
+<script src="/public/scripts/bootstrap/bootstrap.min.js"></script><!-- Bootstrap after jQuery -->
 <script>
 
 
