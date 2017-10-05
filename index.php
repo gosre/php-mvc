b10aa0265a4f68ecf5e42c06d131bf664b522900
Clayton Williams <worldofclayton@gmail.com>
2017-10-05 16:47:52 -0400

    Reorganization

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<?php
-    define("_VALID_PHP", true);
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/public/lib/init.php");
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<head>
-    <title>PHP - MVC</title>
-    <meta name="description" content="Stuff">
-    <meta name="keywords" content="Stuff">
-    <!-- Core Styling -->
-    <link href="/public/style/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="X-UA-Compatible" content="IE=9" /><!-- Dotted Outline fix -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/public/style/theme.css?rand=<?php echo rand(0, 100000); ?>">
-</head>
+
 
 <div id="wrapper">
 
---
+++ templates/default/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<head>
+    <title>PHP - MVC</title>
+    <meta name="description" content="Stuff">
+    <meta name="keywords" content="Stuff">
+    <!-- Core Styling -->
+    <link href="/public/style/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=9" /><!-- Dotted Outline fix -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/public/style/theme.css?rand=<?php echo rand(0, 100000); ?>">
+</head>
+
+<body>
+
